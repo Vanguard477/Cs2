58ca38fe53c31c713fa1bc866c762fb99b7f44ab
revinsd <revinsd456@gmail..com>
2024-11-26 20:54:17 +0500

    1

--- src/main/java/com/trade/cs2/models/Post.java
+++ src/main/java/com/trade/cs2/models/Post.java
@@ -1,9 +1,6 @@
 package com.trade.cs2.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Post {
